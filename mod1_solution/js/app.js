279d2fe4b9d2d7ac0f94feba2220b9251ae47b91
Kevin Lin <kevinlin12052002@gmail.com>
2020-09-08 17:25:00 -0400

    Module 1 Solution

---
+++ mod1_solution/js/app.js
@@ -0,0 +1,26 @@
+(function (){
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', DIController);
+
+    DIController.$inject = ['$scope', '$filter'];
+    function DIController($scope, $filter) {
+        $scope.lunch_items = "";
+        $scope.results = "";
+        $scope.verify = function (){
+            if($scope.lunch_items != ""){
+                var amount = $scope.lunch_items.split(",");
+                if(amount.length <= 3){
+                    $scope.results = "Enjoy!"
+                }
+                else{
+                    $scope.results = "Too Much!"
+                };
+            }
+            else{
+                $scope.results = "Empty"
+            }
+        }
+    }
+})();
